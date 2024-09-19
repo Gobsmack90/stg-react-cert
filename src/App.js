5e27acb5b2c075ff19300da1aad7e4ec2995cfa5
Harrison King <harrison.king@customerdynamics.com>
2024-09-19 17:37:20 -0600

    Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Categories from "./components/Categories";
+import Search from "./components/Search";
+import Jokes from "./components/Jokes";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import NoMatch from "./components/NoMatch";
 
-function App() {
-  return;
-}
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="search" element={<Search />} />
+          <Route path="jokes" element={<Jokes />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
