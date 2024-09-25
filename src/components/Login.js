2fee86b75f95b450cd8b29f833782a7899321c97
Harrison King <harrison.king@customerdynamics.com>
2024-09-24 23:04:51 -0600

    validation complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,16 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../auth";
 import "./Login.css";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [showPass, setShowPass] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+  } = useForm({ mode: "onTouched" });
 
   let navigate = useNavigate();
   let location = useLocation();
@@ -12,13 +19,13 @@ const Login = () => {
 
   let from = location.state?.from?.pathname || "/";
 
-  function handleSubmit(event) {
+  function formSubmit(event) {
     event.preventDefault();
 
     let formData = new FormData(event.currentTarget);
-    let username = formData.get("username");
+    let email = formData.get("email");
 
-    auth.signin(username, () => {
+    auth.signin(email, () => {
       // Send them back to the page they tried to visit when they were
       // redirected to the login page. Use { replace: true } so we don't create
       // another entry in the history stack for the login page.  This means that
@@ -34,13 +41,39 @@ const Login = () => {
       <div className="loginBox">
         <h1 className="title">Welcome to Chuck Norris Jokes</h1>
         <p>You must log in to view the page at {from}</p>
-        <form onSubmit={handleSubmit} className="loginForm">
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+          className="loginForm"
+        >
           <label>
-            Username: <input name="username" type="text" />
+            Email:{" "}
+            <input
+              {...register("email", {
+                required: "Email required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Please enter a valid email.",
+                },
+              })}
+              type="text"
+            />
           </label>
+          <p>{errors.email?.message}</p>
           <label>
             Password:{" "}
-            <input name="password" type={showPass ? "text" : "password"} />{" "}
+            <input
+              {...register("password", {
+                required: "Password required",
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,10}$/gm,
+                  message:
+                    "Password must contain at least 1 capital letter, 1 lower case letter, 1 number, and be between 6 and 10 characters.",
+                },
+              })}
+              type={showPass ? "text" : "password"}
+            />{" "}
             <button
               type="button"
               onClick={() => setShowPass((showPass) => !showPass)}
@@ -48,7 +81,10 @@ const Login = () => {
               {showPass ? "hide" : "show"}
             </button>
           </label>
-          <button type="submit">Login</button>
+          <p>{errors.password?.message}</p>
+          <button type="submit" disabled={!isDirty || !isValid}>
+            Login
+          </button>
         </form>
       </div>
     </div>
